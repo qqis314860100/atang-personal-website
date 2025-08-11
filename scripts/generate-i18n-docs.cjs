ee0882e8d43412f1b142d3f0322513c098afc183
qqis314860100 <ban9ban9da@qq.com>
2025-08-12 00:21:25 +0800

    fix: 优化文件

--- scripts/generate-i18n-docs.cjs
+++ scripts/generate-i18n-docs.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const namespaces = ['common', 'navbar', 'resume', 'dashboard']
+const namespaces = ['common', 'navbar', 'resume', 'dashboard', 'forms', 'login']
 
 async function generateDocs() {
   const docs = {}
--- scripts/start-dev.js
+++ scripts/start-dev.js
@@ -1,6 +1,6 @@
 import { spawn } from 'child_process'
-import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
