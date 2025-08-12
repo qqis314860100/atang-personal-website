df77fab58cdf887d902519bbe5f6231616c02e88
qqis314860100 <ban9ban9da@qq.com>
2025-08-12 19:39:07 +0800

    feat: 更新vercel.json环境变量配置

--- app/login/page.tsx
+++
@@ -1,6 +0,0 @@
-import { routing } from '@/i18n/routing'
-import { redirect } from 'next/navigation'
-
-export default function LoginRootRedirect() {
-  redirect(`/${routing.defaultLocale}/login`)
-}
